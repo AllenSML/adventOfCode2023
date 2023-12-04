60e44bb4874fdb9af8179c609783f4b2589577ad
Shengmao Li <shengmao.li01@sap.com>
2023-12-04 07:35:25 +0100

    Day04 refactor

--- day04.py
+++ day04.py
@@ -8,24 +8,13 @@ file_name ="/input04.txt"
 lines = open(str(cur_dir)+file_name).read().split("\n")
 
 res = 0
-for id, line in enumerate(lines):
-    
-    p1, p2 = [p.strip() for p in line.split(":")[1].strip().split("|")]
-    p_ = len(set(p1.split()) & set(p2.split()))
-
-    res = res + (2**(p_-1) if p_ > 0 else 0)
-
-print("part1:", res)
-
-res = 0
 c2n = [ 1 for _ in range(len(lines))]
+
 for id, line in enumerate(lines):
-    
-    p1, p2 = [p.strip() for p in line.split(":")[1].strip().split("|")]
     p_ = len(functools.reduce(lambda x,y: x & y,   [set(p.strip().split()) for p in line.split(":")[1].strip().split("|")]))
-    
+    res = res + (2**(p_-1) if p_ > 0 else 0)
     for _ in range (p_):
         c2n[_+1+id] += 1 * c2n[id]
 
-print("part2:", sum(c2n))    
-    
+print("part1:", res)
+print("part2:", sum(c2n))

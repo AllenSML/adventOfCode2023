bf836ffdcaed682ed031037ce758c8d66e6c1359
Shengmao Li <shengmao.li01@sap.com>
2023-12-22 23:22:57 +0800

    Day17

--- day17p2.py
+++ day17p2.py
@@ -12,6 +12,7 @@ di = [(0,1), (-1,0), (0,-1), (1,0)]
 heap = [(0, 0, 0, 1), (0,0,0, 2)]
 heapq.heapify(heap)
 
+
 m = len(grid)
 n = len(grid[0])
 seen = set()
@@ -29,11 +30,12 @@ while heap:
     for dd in [dl, dr]:
         dx, dy = di[dd]
         loss = 0
-        for step in [1,2,3]:
+        for step in range(1,11):
             x_, y_ = x + (dx * step), y + (dy * step)
             if x_ < 0 or x_ >= m or y_ < 0 or y_ >= n :
                 continue
             loss += int(grid[x_][y_])
+            if step < 4: continue
             heapq.heappush(heap, (loss+cost, x_, y_, dd))            
             
-print("paert 1: ", ans)
+print("part 2: ", ans)
